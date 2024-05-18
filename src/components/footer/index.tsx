7a0d80d48a03ca8039823c9c6a0dc1294f4ad798
andrebettencourt7 <andreluispb10@gmail.com>
2024-05-17 21:10:02 -0300

    feat: creating footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as S from "./style"
+
+export const Footer: React.FC = () => {
+    return (
+        <S.FooterContainer>
+            <S.FooterDescription>MKS sistemas © Todos os direitos reservados</S.FooterDescription>
+        </S.FooterContainer>
+    )
+}
